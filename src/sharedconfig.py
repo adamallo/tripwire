dd31617928f52a9fb7f558c38d8b1a3beeb82a10
Diego Mallo <dmalload@asu.edu>
2018-05-15 10:36:44 -0700

    Configuration inconsistency

--- src/sharedconfig.py
+++ src/sharedconfig.py
@@ -3,7 +3,7 @@ import imp, re, os
 ##Configuration varibles
 confvars=imp.load_source("config", os.path.dirname(os.path.abspath(__file__))+"/config.txt")
 #applescript=os.path.dirname(os.path.abspath(__file__))+"apple_center_window.osascript" Problem with spaces in the directory name
-applescript='/Users/Diego/Documents/Mis\ cosas/tripwire/src/apple_center_window.osascript'
+applescript=confvars.centerscript
 slackUrl=confvars.url
 slackText=confvars.text
 slackIcon=confvars.icon
